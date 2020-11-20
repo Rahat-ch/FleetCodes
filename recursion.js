ab84e1aa7d99bc2ac0221666530fbc169329d2e7
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2020-11-20 14:54:50 -0500

    recursion solved

--- recursion.js
+++ recursion.js
@@ -1,11 +1,12 @@
 //There are 15 cookies in a cookie jar and I want to
-//eat all of them...hopefully I don't get a stomach ache
-// I'm gonna use recursion to eat them all
-// plz don't tell my wife
+//eat all of them...
 
 const eatCookie = cookie => {
   //cookie eating recursive code goes here
-
+  if (cookie === 0) return
+  cookie = cookie - 1;
+  console.log(`I ate a cookie there are ${cookie} left`);
+  eatCookie(cookie)
 }
 
 eatCookie(15)
